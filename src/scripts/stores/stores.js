01582b6a0318e3e779918cae1b567491bb6e0793
Brent Williams <bwilliams@ad.commsys.com>
2022-08-22 16:00:04 -0400

    Refactored svelte store to get rid of unnecessary variables.

--- src/scripts/stores/stores.js
+++ src/scripts/stores/stores.js
@@ -1,8 +1,4 @@
 import { writable } from "svelte/store";
 
-export const apiStore = writable([]);
-export const queryRes = writable(
-{
-	show: false,
-	data: [],
-});
+const apiModel = { success: false, response:[] };
+export const apiStore = writable(apiModel);
