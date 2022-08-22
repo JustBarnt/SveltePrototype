bf198dca3ed70f94abf4c1ad8c552390b9a9a354
Brent Williams <bwilliams@ad.commsys.com>
2022-08-22 13:12:34 -0400

    Updated packages and configuration for routing.

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,22 @@ export default defineConfig({
 		https: true,
 		port: 2112, 
 	},
+	build: {
+		rollupOptions:{
+			output:{
+				assetFileNames: (assetInfo) => 
+				{
+					let extType = assetInfo.name.split(".").at(1);
+					if(/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType))
+					{
+						extType = "img";
+						return `assets/${extType}/[name]-[hash][extname]`;
+					}
+					return `assets/${extType}/[name][extname]`;	
+
+				},
+				entryFileNames: "assets/js/bundle.js",
+			},
+		},
+	},
 });
