6ad067ff41bd2b0224d4aac7b78c37a06dcbafca
Brent Williams <bwilliams@ad.commsys.com>
2022-08-23 16:19:26 -0400

    Deleted
    Removed because Svelte components from what I can find weren't made to work for routing so all reference of setContext and getContext are not usable as they have to be called during component initialization and are meant to be passed from parent to child (Includes slotted components).

--- src/scripts/MapKeys.js
+++
@@ -1,5 +0,0 @@
-const LicenseKey = Symbol("licenses");
-const FormsKey = Symbol("Forms");
-
-//Object of Symbols for a unique identifier, to make sure the correct context is always aquired.
-export const Keys = { License: LicenseKey, Form: FormsKey };
