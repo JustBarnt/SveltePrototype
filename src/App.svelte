80663710e4a5932482699be604aae798b39ceba7
Brent Williams <bwilliams@ad.commsys.com>
2022-08-23 16:24:25 -0400

    Created routes.js for handling all route functionality.

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,5 @@
 <script>
-	//Routing Imports
-	import router from "page";
-	import Home from "./routes/Home.svelte";
-	import Licenses from "./routes/Licenses.svelte";
-
-	let page;
-
-	router("/", () => page = Home);
-	router("/Licenses", () => page = Licenses);
-	router.start();
+	import { page } from "./routes";
 </script>
 
 <h1>Licensing Web Tool</h1>
