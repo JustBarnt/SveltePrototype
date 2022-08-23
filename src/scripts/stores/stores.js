62e059e46edbdc41a885f3edc74e5b9b5bbf2083
Brent Williams <bwilliams@ad.commsys.com>
2022-08-23 16:27:26 -0400

    Removed the store for FORMS

--- src/scripts/stores/stores.js
+++ src/scripts/stores/stores.js
@@ -2,4 +2,3 @@ import { writable } from "svelte/store";
 
 const apiModel = { success: false, response:[] };
 export const API = writable(apiModel);
-export const FORMS = writable({ amount: 10, column: "createdby", value: "joshr", startDate:"", endDate:"" });
