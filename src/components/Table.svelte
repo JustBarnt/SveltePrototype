46127cfdb502ad3a329801230c8ab95a79e8919d
Brent Williams <bwilliams@ad.commsys.com>
2022-08-25 12:21:24 -0400

    Created functionality for clicking on a row and gathering the PK of the license.

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -1,10 +1,45 @@
 <script>
-	import { Utilities } from "../scripts/utilities/Utilities";
+import { onMount } from "svelte";
+import { CreateInputForm } from "../scripts/CreateInputForm";
+import { Utilities } from "../scripts/utilities/Utilities";
 	export let data = undefined;
 
-	function HandleLicenseSelect()
+	const tablePKs = [];
+	let activeRow = 0;
+		
+	onMount( () => 
 	{
-		console.log("License clicked.");
+		if(data !== undefined)
+		{
+			data.forEach((license) => 
+			{
+				for (const key in license)
+				{
+					if (Object.hasOwnProperty.call(license, key))
+					{
+						tablePKs.push(license["id"]);
+						break;
+					}
+				}
+			});
+		}
+	});
+
+	function HandleLicenseSelect(event)
+	{
+		activeRow = GetRowNum(event.currentTarget);
+		let form = CreateInputForm.Form("LicenseForm", "licenseForm", "/license", "GET");
+		let input = CreateInputForm.Input("text", "selector", tablePKs[activeRow]);
+		form.append(input);
+		document.getElementById("TableContainer").append(form);
+
+		// @ts-ignore submit is a valid method on forms. @tslint is dumb.
+		document.getElementById("LicenseForm").submit(event);
+	}
+
+	function GetRowNum(element)
+	{
+		return element.closest("tr").rowIndex;
 	}
 </script>
 
@@ -12,14 +47,14 @@
 	<table id="Licenses">
 		<thead>	
 			{#each Object.keys(data[0]) as Header}
-				<th>{Utilities.FormatColumnHeader(Header)}</th>
+				<th scope="col" headers={Header.toLowerCase()}>{Utilities.FormatColumnHeader(Header)}</th>
 			{/each}
 		</thead>
 		<tbody>
 			{#each data as query}
-				<tr on:click={() => HandleLicenseSelect()}>
-					{#each Object.values(query) as Values}
-							<td>{Values}</td>
+				<tr on:click={(event) => HandleLicenseSelect(event)}>
+					{#each Object.values(query) as Values, index}
+						<td headers={Object.keys(data[0])[index]} >{Values}</td>
 					{/each}
 				</tr>
 			{/each}
@@ -35,6 +70,10 @@
 		box-shadow: 0 0 2rem rgba(0,0,0,0.35);
 		overflow:hidden;
 		color:#fff;
+	}
+
+	#Licenses th[headers="id"], #Licenses td[headers="id"]{
+		display:none;
 	}
 	
 	#Licenses thead th, #Licenses tbody td{
