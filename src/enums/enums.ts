cf1e21ab91c99ef1cdfca81c28a7c864430115ae
Brent Williams <bwilliams@ad.commsys.com>
2022-09-09 16:40:44 -0400

    Moved enums to their own folder.

--- src/enums/enums.ts
+++ src/enums/enums.ts
@@ -1,9 +1,15 @@
 export enum Colors
-	{
+{
 		BLUE = "#4967ec",
 		GREEN = "#6dbaa1",
 		GREY = "#aba9bb",
 		DARKGREY = "#35343d",
 		RED = "#e01f41",
 		PURPLE = "#9f64ff"
-	}
+}
+
+export enum AlertVisibility
+{
+	Visible = "visible",
+	Hidden = "hidden"
+}
