e01bb8acb55e136f990bca9acbafe068e5590643
Brent Williams <bwilliams@ad.commsys.com>
2022-09-15 10:34:52 -0400

    renamed stores, removed unused store, added a store for prev_query ran.

--- src/scripts/stores/stores.ts
+++ src/scripts/stores/stores.ts
@@ -5,7 +5,6 @@ let token: string | null = null;
 let authSession: typeof localStorage;
 // let userLocation: ILocation
 
-export const LICENSE_STORE = writable(api);
-export const USER_SESSION = writable(token);
-export const AUTH_SESSION = writable(authSession);
-export const USER_LOCATION = writable();
+export const LICENSES = writable<IResponse>(api);
+export const USER_SESSION = writable<typeof token>(token);
+export const PREV_QUERY = writable<string>("");
