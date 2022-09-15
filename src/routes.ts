0a54784183365928a6cf5ee1eecfca0c1ceb4eef
Brent Williams <bwilliams@ad.commsys.com>
2022-09-15 10:33:16 -0400

    moved into services.

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import Home from "@pages/Home.svelte";
-import Login from "@pages/Login.svelte";
-import Response from "@pages/Response.svelte";
-
-export const routes = {
-	"/": Login,
-	"/home": Home,
-	"/licenses": Response
-};
