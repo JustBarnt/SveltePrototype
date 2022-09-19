66c05cd781fc289b5d7295dc8192686270e559ea
Brent Williams <bwilliams@ad.commsys.com>
2022-09-19 15:44:06 -0400

    New component for nav

---
+++ src/components/NavButton.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	export let text:string;
+	export let gridArea:string;
+	
+	const navDispatch = createEventDispatcher();
+	const HandleNavigation = () => navDispatch("navigation");
+</script>
+
+<p class="nav-item" style:grid-area={gridArea} on:click on:click={HandleNavigation}>{text}</p>
+
+<style lang="scss">
+	.nav-item{
+			color: white;
+			text-decoration: none;
+			font-weight: 600;
+			border-radius: $borderRadius;
+			border: 0.15rem solid transparent;
+			transition: all 0.25s;
+			padding: 0.5rem;
+			cursor: pointer;
+
+			&:hover, &:focus{
+				padding: 0.5rem;
+				border-color: $darkGrey;
+				background-color: #30303050;
+			}
+		}
+</style>
